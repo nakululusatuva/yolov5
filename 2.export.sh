2333b67ba643646441c79924e17c7957f1bf6a03
nakululusatuva <yirainsuen@gmail.com>
2025-11-19 09:51:34 +0800

    Add option '--rknpu' to '2.export.sh'

--- 2.export.sh
+++ 2.export.sh
@@ -1,8 +1,6 @@
 # rknn-toolkit2 supports opset range 12~19
 python export.py \
-  --weights /home/nova/Repos/yolov5/runs/train-seg/exp4/weights.230/best.pt \
+  --rknpu \
+  --weights /home/nova/Repos/yolov5/runs/train-seg/exp/weights.230/best.pt \
   --include onnx \
-  --opset 18 \
-  --simplify \
-  --imgsz 640 640 \
-  --batch 1
+  --opset 18
